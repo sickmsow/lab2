9bc095e3ceddd2ef7d8fb0588b56d9e3f349e5f5
sickmsow <sickmsow@gmail.com>
2025-02-13 16:08:13 +0000

    sigh

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.get('/api/greeting', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
 });
 
-
-app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+module.exports = app;
