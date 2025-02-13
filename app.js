7be86cacb8e16deb59b5a1b0d809eede2ced57a2
sickmsow <sickmsow@gmail.com>
2025-02-13 16:03:41 +0000

    sigh

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
-
 const app = express();
-const PORT = 3000;
-
 
 app.use(cors());
+app.use(express.json());
+
 
 
 app.use(express.static(path.join(__dirname, 'public')));
